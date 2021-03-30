477e8f58ba07709fafa287848664e7fd8e034478
Alex <alex+git@fremantle.io>
2021-03-29 19:02:05 -0700

    Update webpack config for new postcss-loader

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -40,7 +40,21 @@ module.exports = (_env, options) => {
           use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
-            'postcss-loader',
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      "postcss-preset-env",
+                      {
+                        // Options
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
             'sass-loader',
           ],
         }
