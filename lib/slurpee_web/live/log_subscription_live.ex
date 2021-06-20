6b6c725f27d36c3253e13792a5484b2da3910047
Alex <alex+git@fremantle.io>
2021-06-19 17:38:36 -0700

    Add search to log subscriptions

--- lib/slurpee_web/live/log_subscription_live.ex
+++ lib/slurpee_web/live/log_subscription_live.ex
@@ -1,14 +1,79 @@
 defmodule SlurpeeWeb.LogSubscriptionLive do
   use SlurpeeWeb, :live_view
+  import SlurpeeWeb.ViewHelpers.SearchQueryHelper, only: [assign_search_query: 2]
 
   @impl true
   def mount(_params, _session, socket) do
-    log_subscriptions = Slurp.Commander.log_subscriptions([])
-
     socket =
       socket
-      |> assign(log_subscriptions: log_subscriptions)
+      |> assign(:query, nil)
 
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(params, _uri, socket) do
+    socket =
+      socket
+      |> assign_search_query(params)
+      |> assign_search()
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("search", params, socket) do
+    socket =
+      socket
+      |> assign_search_query(params)
+      |> send_search_after(200)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:search, socket) do
+    socket =
+      socket
+      |> assign(:search_timer, nil)
+      |> assign_search()
+
+    {:noreply, socket}
+  end
+
+  defp send_search_after(socket, after_ms) do
+    if socket.assigns[:search_timer] do
+      socket
+    else
+      timer = Process.send_after(self(), :search, after_ms)
+      assign(socket, :search_timer, timer)
+    end
+  end
+
+  defp assign_search(socket) do
+    socket
+    |> assign(log_subscriptions: search_log_subscriptions(socket.assigns.query))
+  end
+
+  defp search_log_subscriptions(search_term) do
+    []
+    |> Slurp.Commander.log_subscriptions()
+    |> search_log_subscriptions(search_term)
+  end
+
+  defp search_log_subscriptions(log_subscriptions, nil) do
+    log_subscriptions
+  end
+
+  defp search_log_subscriptions(log_subscriptions, search_term) do
+    log_subscriptions
+    |> Enum.filter(fn s ->
+      String.contains?(s.blockchain_id, search_term) ||
+      String.contains?(s.event_signature, search_term) ||
+      String.contains?(s.hashed_event_signature, search_term) ||
+      String.contains?(s.enabled |> to_string(), search_term) ||
+      String.contains?(s.struct |> Atom.to_string(), search_term) ||
+      String.contains?(s.handler |> inspect(), search_term)
+    end)
   end
 end
---
+++ lib/slurpee_web/view_helpers/search_query_helper.ex
@@ -0,0 +1,14 @@
+defmodule SlurpeeWeb.ViewHelpers.SearchQueryHelper do
+  def assign_search_query(conn, params) do
+    query = extract_query(params, conn)
+    Phoenix.LiveView.assign(conn, :query, query)
+  end
+
+  defp extract_query(params, conn) do
+    case Map.get(params, "query") do
+      "" -> nil
+      nil -> conn.assigns.query
+      query -> query
+    end
+  end
+end
