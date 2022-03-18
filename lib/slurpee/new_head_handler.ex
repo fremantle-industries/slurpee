309ad34fc2ee9d9a828e0296965dfd7c29af0df7
Alex Kwiatkowski <alex+git@fremantle.io>
2022-03-17 20:34:00 -0700

    Add behavior callback to Slurpee.NewHeadHandler

--- lib/slurpee/new_head_handler.ex
+++ lib/slurpee/new_head_handler.ex
@@ -1,4 +1,9 @@
 defmodule Slurpee.NewHeadHandler do
+  @type blockchain :: Slurp.Blockchains.Blockchain.id()
+  @type block_number :: Slurp.Adapter.block_number()
+
+  @callback handle_new_head(blockchain, block_number) :: no_return
+
   require Logger
 
   def handle_new_head(blockchain, block_number) do
